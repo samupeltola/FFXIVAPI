be71bba0049774caee3f8f111a2087f69ce00bdd
Samu <samu.peltola94@gmail.com>
2020-08-25 14:24:45 +0300

    working API connection with fetch

--- ffxivapi/src/components/Character.js
+++ ffxivapi/src/components/Character.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+//import ReactDOM from 'react-dom'
 import './Character.css'
 import XIVAPI from 'xivapi-js'
 const xiv = new XIVAPI()
-
-
 
 class Character extends Component {
 /*  to display character info with fetch. Character ID: 8729279
@@ -17,10 +15,32 @@ FC	Free Company
 FCM	Free Company Members
 PVP	PVP Team
 */
+
+state = {
+    loading: true,
+    character: null
+};
+
+async componentDidMount() {
+    const url = "https://xivapi.com/character/8729279?data=CJ";
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.Character);
+    this.setState({ Character: data.Character, loading: false })
+}
+
     render() {
         return (
             <div>
-            
+                {this.state.loading || !this.state.Character ? (
+                <div className="Character">loading...</div>
+                ) : (
+                    <div className="Character">
+                        <div className="CharacterName"> {this.state.Character.Name} </div>
+                        <img src={this.state.Character.Avatar} alt="Character Icon"/>
+                        <div>Current class: { this.state.Character.ActiveClassJob.UnlockedState.Name } </div>
+                    </div>
+                )}
             </div>
         );
     }
